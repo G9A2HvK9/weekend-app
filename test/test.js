dd69405fa853987e8786772141be9381b8c8b0fb
Filipe Fasolin <ffasolin@gmail.com>
2017-06-05 17:49:31 +0100

    Fixes asynchronous issue with test

--- test/test.js
+++ test/test.js
@@ -15,15 +15,15 @@
 // });
 const app = require('../index.js');
 const assert = require('assert');
-
 const Browser = require('zombie');
 const url = "http://localhost:3000/";
 const browser = new Browser();
 
 describe('Test', function() {
-  it('returns Hello World!', function() {
-    browser.visit(url, function() {
-      browser.assert.text('body', 'This is not right!');
+  it('returns Hello World!', function(done) {
+    browser.visit(url).then(function() {
+      browser.assert.text('body', 'Hello World!');
+      done();
     });
   });
 });
