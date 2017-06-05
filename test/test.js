69a3d5d4c37ee7a76ed4dfa02034b1bb6f794ca5
Bailey Kursar <bkursar@gmail.com>
2017-06-05 17:40:58 +0100

    Trying to get Zombie to work

--- test/test.js
+++ test/test.js
@@ -1,15 +1,29 @@
-var assert = require('assert');
+// var assert = require('assert');
+// const Browser = require('zombie');
+// Browser.localhost('http://localhost:3000/', 3000)
+//
+// describe('Main', function() {
+//   const browser = new Browser();
+//
+//   describe('#index()', function() {
+//     it('return Hello World!', function() {
+//       browser.visit('/', function() {
+//         browser.assert.status(404);
+//       });
+//     });
+//   });
+// });
+const app = require('../index.js');
+const assert = require('assert');
+
 const Browser = require('zombie');
-const url = 'http://localhost:3000/'
+const url = "http://localhost:3000/";
+const browser = new Browser();
 
-describe('Main', function() {
-  const browser = new Browser();
-
-  describe('#index()', function() {
-    it('return Hello World!', function() {
-      browser.visit(url, function() {
-        browser.assert.text('hieee');
-      });
+describe('Test', function() {
+  it('returns Hello World!', function() {
+    browser.visit(url, function() {
+      browser.assert.text('body', 'This is not right!');
     });
   });
 });
