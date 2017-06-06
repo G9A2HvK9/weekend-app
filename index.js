7c08ca353ebbb0b892da151c4db971659108797f
Bailey Kursar <bkursar@gmail.com>
2017-06-06 12:28:23 +0100

    Made our first view work

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('views/main');
+  res.render('index');
 })
 
 app.listen(3000, function () {
