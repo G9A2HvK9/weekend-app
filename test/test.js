17e0be7c7e2a0f90ab3e78b84840d1f2b23d58d2
Filipe Fasolin <ffasolin@gmail.com>
2017-06-06 15:57:40 +0100

    Adds service to travis file

--- test/test.js
+++ test/test.js
@@ -3,7 +3,6 @@ const assert = require('assert');
 const Browser = require('zombie');
 const url = "http://localhost:3000/";
 const browser = new Browser();
-var activitiesSchema = require('../db/schema')
 
 describe('Test', function() {
   it('returns Hello World!', function(done) {
