b13eb259f30f48f0c6482c30fdbc204ecf340e09
isabellarner <larner.isabel@gmail.com>
2017-06-06 12:15:56 +0100

    begin view rendering

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('views/main');
 })
 
 app.listen(3000, function () {
