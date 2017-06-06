0b77544814b772ba1b8abdc8a131d33dfc0b4a64
Ben Taylor <taylor.ben89@gmail.com>
2017-06-06 15:53:11 +0100

    possibly react?

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
+//
+// app.set('view engine', 'ejs');
+//
+// app.get('/', function (req, res) {
+//   res.render('main', { title: 'Hello World!' });
+// })
+//
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
 
-app.set('view engine', 'ejs');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-app.get('/', function (req, res) {
-  res.render('main', { title: 'Hello World!' });
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
