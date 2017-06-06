a917a0dbcb622b97b5f88f45bdcdbfd79092bbcd
Bailey Kursar <bkursar@gmail.com>
2017-06-06 12:52:42 +0100

    Trying to put a javascript script to work inside the ejs file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('main', { title: 'Hello World!' });
 })
 
 app.listen(3000, function () {
