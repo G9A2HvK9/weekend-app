a3263e6192dfa6b78b45127d5bab1275488933fb
Filipe Fasolin <ffasolin@gmail.com>
2017-06-06 16:06:40 +0100

    Creates db entry for test

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ const assert = require('assert');
 const Browser = require('zombie');
 const url = "http://localhost:3000/";
 const browser = new Browser();
+var ActivitiesModel = require('../db/schema.js');
 
 describe('Test', function() {
   it('returns Hello World!', function(done) {
@@ -15,6 +16,7 @@ describe('Test', function() {
 
 describe('Activities schema', function() {
   it('retrieves activities list from database', function(done) {
+    ActivitiesModel.create({name: 'Football', cost: 60, tubeStation: 'Hackney Central'})
     browser.visit(url + 'activities').then(function() {
       browser.assert.text('body', 'Football');
       done();
