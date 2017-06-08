15d8c3f698dd3d0657cdd621f491f092df1c40d7
hendrik <g9a2hvk9@me.com>
2017-06-08 14:17:15 +0100

    Styles slide show

--- src/index.js
+++ src/index.js
@@ -15,17 +15,20 @@ import Slider from 'react-slick';
 
 class ResultsSlider extends React.Component {
   render() {
+    var imgSrc = "http://dontpaniconline.com/media/magazine/body/2015-04-10/images/20150409_MakersAcademy_0072.jpg";
     var settings = {
     dots: true,
+    swipeToSlide: true,
     };
     return (
       <Slider {...settings}>
-        <div class="searchResult"><h3>1</h3></div>
-        <div class="searchResult"><h3>2</h3></div>
-        <div class="searchResult"><h3>3</h3></div>
-        <div class="searchResult"><h3>4</h3></div>
-        <div class="searchResult"><h3>5</h3></div>
-        <div class="searchResult"><h3>6</h3></div>
+        <div style={{height: 350, width: 900, overflow: false}}><img src= {imgSrc}></img></div>
+        <div style={{height: 350, width: 900, overflow: false}}><img src="http://www.telegraph.co.uk/content/dam/video_previews/x/5/x5cgi0ode66q6vuxezqmehmexwer6bt-xlarge.jpg"></img></div>
+        <div style={{height: 350, width: 900, overflow: false}}><img src="http://www.telegraph.co.uk/content/dam/video_previews/x/5/x5cgi0ode66q6vuxezqmehmexwer6bt-xlarge.jpg"></img></div>
+        <div style={{height: 350, width: 900, overflow: false}}><img src="http://www.telegraph.co.uk/content/dam/video_previews/x/5/x5cgi0ode66q6vuxezqmehmexwer6bt-xlarge.jpg"></img></div>
+        <div style={{height: 350, width: 900, overflow: false}}><img src="http://www.telegraph.co.uk/content/dam/video_previews/x/5/x5cgi0ode66q6vuxezqmehmexwer6bt-xlarge.jpg"></img></div>
+        <div style={{height: 350, width: 900, overflow: false}}><img src="http://www.telegraph.co.uk/content/dam/video_previews/x/5/x5cgi0ode66q6vuxezqmehmexwer6bt-xlarge.jpg"></img></div>
+        <div style={{height: 350, width: 900, overflow: false}}><img src="http://www.telegraph.co.uk/content/dam/video_previews/x/5/x5cgi0ode66q6vuxezqmehmexwer6bt-xlarge.jpg"></img></div>
       </Slider>
     );
   }
