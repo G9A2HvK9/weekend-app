03a7289d2d03dd7494d47c198494e2f50a942939
Filipe Fasolin <ffasolin@gmail.com>
2017-06-08 09:55:00 +0100

    Test for map displayed on screen

--- test/test.js
+++ test/test.js
@@ -5,21 +5,20 @@ const url = "http://localhost:3000/";
 const browser = new Browser();
 var ActivitiesModel = require('../db/schema.js');
 
-describe('Test', function() {
-  it('returns Hello World!', function(done) {
-    browser.visit(url).then(function() {
-      browser.assert.text('body', 'Hello World!');
-      done();
-    });
-  });
-});
-
 describe('Activities schema', function() {
   it('retrieves activities list from database', function(done) {
     ActivitiesModel.create({name: 'Football', cost: 60, tubeStation: 'Hackney Central'})
     browser.visit(url + 'activities').then(function() {
       browser.assert.text('body', 'Football');
       done();
+    });
+  });
+});
+
+describe('Home page', function() {
+  it('has a map displayed on screen', function() {
+    browser.visit(url).then(function() {
+      browser.assert.element('#map');
     });
   });
 });
